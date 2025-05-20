0a7a4d663b1af82fcdf6b4d9849eeb55c2aed393
Manveer <manveer.vinnisoft@gmail.com>
2025-05-20 17:51:57 +0530

    changes

--- src/store/actions/userActions.js
+++ src/store/actions/userActions.js
@@ -1,48 +1,40 @@
 import api from "../../services/api";
 import endPoints from "../../services/endPoints";
-import {
-  handleFormatDate,
-  showCatchErrorToast,
-} from "../../utils/commonFunctions";
-import { setChild, setChildrenList } from "../slices/childSlice";
+import { showCatchErrorToast } from "../../utils/commonFunctions";
+import { setChildrenList } from "../slices/childSlice";
 import { showToastAction } from "../slices/commonSlice";
 import { setUserProfile } from "../slices/userSlice";
 
-export const addUserAction = (data, setLoading, onRes) => async (dispatch) => {
-  try {
-    setLoading(true);
-    const res = await api(
-      "post",
-      endPoints.USERS,
-      data,
-      {},
-      "multipart/form-data"
-    );
+export const sendUserInviteAction =
+  (data, setLoading, onRes) => async (dispatch) => {
+    try {
+      setLoading(true);
+      const res = await api("post", endPoints.INVITE, data);
 
-    if (res.success) {
-      dispatch(
-        showToastAction({
-          type: "success",
-          title: "Success",
-          detail: res.message,
-        })
-      );
-      onRes && onRes(res);
-    } else {
-      dispatch(
-        showToastAction({
-          type: "error",
-          title: "Error",
-          detail: res.message,
-        })
-      );
+      if (res.success) {
+        dispatch(
+          showToastAction({
+            type: "success",
+            title: "Success",
+            detail: res.message,
+          })
+        );
+        onRes && onRes(res);
+      } else {
+        dispatch(
+          showToastAction({
+            type: "error",
+            title: "Error",
+            detail: res.message,
+          })
+        );
+      }
+    } catch (error) {
+      showCatchErrorToast();
+    } finally {
+      setLoading(false);
     }
-  } catch (error) {
-    showCatchErrorToast();
-  } finally {
-    setLoading(false);
-  }
-};
+  };
 
 export const getUserListAction =
   (page, rows, setLoading = () => {}, onRes) =>
@@ -70,27 +62,6 @@ export const getUserListAction =
       setLoading(false);
     }
   };
-
-export const getUserDataAction = (id) => async (dispatch) => {
-  try {
-    const result = await api("get", endPoints.USERS + `${id}/`);
-    if (result?.success) {
-      dispatch(setChild(result?.data));
-      return onRes && onRes(result?.data);
-    } else {
-      dispatch(
-        showToastAction({
-          type: "error",
-          title: "Error",
-          detail: result?.message,
-        })
-      );
-    }
-  } catch (error) {
-    showCatchErrorToast();
-  } finally {
-  }
-};
 
 export const deleteUserAction = (id, setLoading, onRes) => async (dispatch) => {
   try {
@@ -122,50 +93,6 @@ export const deleteUserAction = (id, setLoading, onRes) => async (dispatch) => {
   }
 };
 
-export const updateUserAction =
-  (id, data, setLoading, onRes) => async (dispatch) => {
-    try {
-      setLoading(true);
-      const formData = new FormData();
-      formData.append("full_name", data.fullName);
-      formData.append("date_of_birth", handleFormatDate(data.dateOfBirth));
-      formData.append("grade_level", data.gradeLevel);
-      if (typeof data.image !== "string") {
-        formData.append("image", data.image);
-      }
-      const res = await api(
-        "put",
-        endPoints.USERS + `${id}/`,
-        formData,
-        {},
-        "multipart/form-data"
-      );
-
-      if (res.success) {
-        dispatch(
-          showToastAction({
-            type: "success",
-            title: "Success",
-            detail: res.message,
-          })
-        );
-        onRes && onRes(res);
-      } else {
-        dispatch(
-          showToastAction({
-            type: "error",
-            title: "Error",
-            detail: res?.message,
-          })
-        );
-      }
-    } catch (error) {
-      showCatchErrorToast();
-    } finally {
-      setLoading(false);
-    }
-  };
-
 export const getProfileAction = (onRes) => async (dispatch) => {
   const res = await api("get", endPoints.GET_PROFILE);
   if (res.success) {
@@ -173,58 +100,6 @@ export const getProfileAction = (onRes) => async (dispatch) => {
     onRes && onRes(res?.data);
   }
 };
-
-export const updateProfileAction =
-  (data, setLoading, onRes) => async (dispatch) => {
-    try {
-      setLoading(true);
-      const formData = new FormData();
-      formData.append("full_name", data.fullName);
-      formData.append("email", data.email);
-      if (typeof data.image === "object") {
-        formData.append("image", data.image);
-      }
-
-      const res = await api(
-        "put",
-        endPoints.EDIT_PROFILE,
-        formData,
-        {},
-        "multipart/form-data"
-      );
-
-      if (res.success) {
-        dispatch(
-          showToastAction({
-            type: "success",
-            title: res.message || "Profile updated successfully",
-            detail: "The profile has been updated",
-          })
-        );
-        dispatch(setUserProfile(res?.data));
-        onRes && onRes(res);
-      } else {
-        dispatch(
-          showToastAction({
-            type: "error",
-            title: res.message || "Failed to add profile",
-            detail: "Please check the information and try again",
-          })
-        );
-      }
-    } catch (error) {
-      dispatch(
-        showToastAction({
-          type: "error",
-          title: "Something went wrong",
-          detail:
-            error.message || "Failed to add profile due to technical issues",
-        })
-      );
-    } finally {
-      setLoading(false);
-    }
-  };
 
 export const updateUserActiveStatus =
   (id, setLoading, onRes) => async (dispatch) => {
--- src/views/users/add.jsx
+++ src/views/users/add.jsx
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import PageTitleHeader from "../../shared/component/PageTitleHeader";
-import { CustomInput, CustomPassword } from "../../shared/component/AllInputs";
-import { useNavigate, useParams } from "react-router-dom";
+import { CustomInput } from "../../shared/component/AllInputs";
+import { useNavigate } from "react-router-dom";
 import formValidation from "../../utils/validations";
 import { showFormErrors } from "../../utils/commonFunctions";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import PrimaryButton from "../../shared/component/CustomButton";
-import { addUserAction, getUserDataAction } from "../../store/actions/userActions";
+import {
+  sendUserInviteAction,
+} from "../../store/actions/userActions";
 
 export default function AddUser() {
-  const { id } = useParams();
-  const formText = id ? "Edit" : "Add";
-  const items = [{ label: `${formText} User` }];
+  const items = [{ label: `Invite User` }];
   const home = {
     label: "Users",
     url: "#/users",
@@ -19,26 +19,10 @@ export default function AddUser() {
 
   const navigate = useNavigate();
   const [data, setData] = useState({
-    name: "",
     email: "",
-    password: "",
   });
   const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
-
-  useEffect(() => {
-    if (id) {
-      getUserDataAction(id, (res) => {
-        if (res?.success) {
-          setData({
-            name: generatedLessonResponse?.name || "",
-            email: generatedLessonResponse?.email || "",
-            password: generatedLessonResponse?.password || "",
-          });
-        }
-      });
-    }
-  }, [id]);
 
   const handleChange = ({ name, value }) => {
     const formErrors = formValidation(name, value, data);
@@ -48,7 +32,7 @@ export default function AddUser() {
   const handleSubmit = () => {
     if (showFormErrors(data, setData)) {
       dispatch(
-        addUserAction(data, setLoading, (res) => {
+        sendUserInviteAction(data, setLoading, (res) => {
           if (res?.success) {
             navigate("/users");
           }
@@ -60,21 +44,13 @@ export default function AddUser() {
   return (
     <>
       <PageTitleHeader
-        title={`${formText} User`}
+        title={`Invite User`}
         model={items}
         home={home}
         label={items[0].label}
       />
       <div className="p-4 shadow-1 border-round-xl">
         <div className="grid">
-          <CustomInput
-            name="name"
-            className="w-full"
-            data={data}
-            onChange={handleChange}
-            col={6}
-            extraClassName="px-2 md:w-6 w-full"
-          />
           <CustomInput
             name="email"
             className="w-full"
@@ -83,20 +59,10 @@ export default function AddUser() {
             col={6}
             extraClassName="px-2 md:w-6 w-full"
           />
-          <CustomPassword
-            name="password"
-            className="w-full"
-            label="Password"
-            placeholder="Password"
-            col={6}
-            onChange={handleChange}
-            data={data}
-            extraClassName="px-2 md:w-6 w-full"
-          />
         </div>
         <div className="flex justify-content-end mt-5 gap-3">
           <PrimaryButton
-            label="Add User"
+            label="Invite User"
             extraClassNames="bg-dark-red"
             onClick={handleSubmit}
             loading={loading}
